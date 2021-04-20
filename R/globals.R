d8d1c67a8953862e45a43a1504e4b987a28f2726
Marianna Plucinska <marianna.plucinska@roche.com>
2021-04-20 09:50:05 +0200

    Minor

--- R/globals.R
+++ R/globals.R
@@ -1,7 +1,7 @@
-
 #' Globals variables
 #' 
 #' Save xna_dictionary and complementary_bases as global variables
 #' 
 #' @importFrom utils globalVariables
+#' @export
 utils::globalVariables(c("xna_dictionary", "complementary_bases"))
